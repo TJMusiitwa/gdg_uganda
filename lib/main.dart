f86c14105e9d4bb400f7c5a33aaf8cc845d51af4
TJMusiitwa <jonamusiitwa@outlook.com>
2024-11-10 22:18:17 +0300

    🔍 ✨ Update main.dart to enhance app metadata and styling

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,72 @@ import 'package:jaspr/server.dart';
 
 // Imports the [App] component.
 import 'app.dart';
-
 // This file is generated automatically by Jaspr, do not remove or edit.
 import 'jaspr_options.dart';
 
 void main() {
   // Initializes the server environment with the generated default options.
-  Jaspr.initializeApp(
-    options: defaultJasprOptions,
-  );
+  Jaspr.initializeApp(options: defaultJasprOptions);
 
   // Starts the app.
   //
   // [Document] renders the root document structure (<html>, <head> and <body>)
   // with the provided parameters and components.
   runApp(Document(
-    title: 'gdg_uganda',
-    styles: [
-      // Special import rule to include to another css file.
-      css.import('https://fonts.googleapis.com/css?family=Roboto'),
-      // Each style rule takes a valid css selector and a set of styles.
-      // Styles are defined using type-safe css bindings and can be freely chained and nested.
-      css('html, body')
-          .text(fontFamily: const FontFamily.list([FontFamily('Roboto'), FontFamilies.sansSerif]))
-          .box(width: 100.percent, minHeight: 100.vh)
-          .box(margin: EdgeInsets.zero, padding: EdgeInsets.zero),
-      css('h1').text(fontSize: 4.rem).box(margin: EdgeInsets.unset),
+    title: 'GDGUganda',
+    viewport: 'width=device-width, initial-scale=1',
+    head: [
+      link(
+          href:
+              'https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css',
+          rel: 'stylesheet',
+          type: 'text/css'),
+      script(src: 'https://cdn.tailwindcss.com', []),
+      link(rel: 'shortcut icon', href: 'favicon.ico'),
+      link(
+          rel: 'icon',
+          type: 'image/png',
+          href: 'favicon-96x96.png',
+          attributes: {'sizes': '96x96'}),
+      link(rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg'),
+      link(
+        rel: 'apple-touch-icon',
+        href: 'apple-touch-icon.png',
+        attributes: {'sizes': '180x180'},
+      ),
+      link(
+        rel: 'manifest',
+        href: 'site.webmanifest',
+      ),
     ],
+    meta: {
+      'title': 'GDGUganda',
+      'description':
+          'A demo site for GDG Uganda, showcasing the power of Jaspr for building modern web applications.',
+      'author': 'Jonathan Musiitwa',
+      'publisher': 'Jonathan Musiitwa',
+      'creator': 'Jonathan Musiitwa',
+      'keywords': 'GDG, Uganda, GDG Uganda, Flutter, Dart, Jaspr',
+      'robots': 'noindex, nofollow',
+      //open graph
+      'og:title': 'GDGUganda',
+      'og:description':
+          'A demo site for GDG Uganda, showcasing the power of Jaspr for building modern web applications.',
+      'og:type': 'website',
+      'og:url': 'https://gdgkampala-1570452632700.web.app/',
+      'og:image':
+          'https://gdgkampala-1570452632700.web.app/images/gdg-logo.png',
+      'og:site_name': 'Project Management Tools Expo 2025',
+      'og:locale': 'en_GB',
+      //twitter
+      'twitter:card': 'summary_large_image',
+      'twitter:title': 'GDGUganda',
+      'twitter:url': 'https://gdgkampala-1570452632700.web.app/',
+      'twitter:description':
+          'A demo site for GDG Uganda, showcasing the power of Jaspr for building modern web applications.',
+      'twitter:image':
+          'https://gdgkampala-1570452632700.web.app/images/gdg-logo.png',
+    },
     body: App(),
   ));
 }
